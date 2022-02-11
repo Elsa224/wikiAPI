9e44317b3564de98105edf7787aab42b996fe918
Elsa Zougouri <elsazougouri224@gmail.com>
2022-02-11 16:07:46 +0000

    Added POST /articles and DELETE /articles - Refactoring code by adding app.route - Added GET /articles/:articleTitle

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require( "express" );
 const bodyParser = require( "body-parser" );
 const ejs = require( "ejs" );
 const mongoose = require( "mongoose" );
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 
 //Creating an app constant and use EJS as its view engine
 const app = express(  );
@@ -31,16 +33,52 @@ const articleSchema = mongoose.Schema( {
 //Model
 const Article = mongoose.model( "Article", articleSchema );
 
+/// ----------------------------------- ROUTE /articles ----------------------------------- ///
+app.route( "/articles" )
+    .get( ( req, res ) => {
+        Article.find( ( error, foundArticles ) => {
+            if ( !error )
+                res.send( { error: false, message: "Articles found", data : foundArticles } );
+            else    
+                res.send( { error: true, message: error } );
+        } );
+    } )
 
-//GET the articles
-app.get( "/articles", ( req, res ) => {
-    Article.find( ( error, foundArticles ) => {
-        if ( !error )
-            res.send( foundArticles );
-        else    
-            console.log( error );
+    .post(  ( req, res ) => {
+        const newArticle = new Article( {
+            title: req.body.title,
+            content: req.body.content
+        } );
+
+        newArticle.save( ( error ) => {
+            if ( !error )
+                res.send( { error: false, message: "Article successfully created !" } );
+            else
+                res.send({ error: true, message: error } );
+        } );
+    } )
+
+    .delete( ( req, res ) => {
+        Article.deleteMany( ( error ) => {
+            if ( !error )
+                res.send( { error: false, message: "All the articles successfully deleted !" } );
+            else
+                res.send({ error: true, message: error } );
+        } );
     } );
-} );
+
+/// ----------------------------------- ROUTE /articles/{something} ----------------------------------- ///
+app.route( "/articles/:articleTitle" )
+    .get( ( req, res ) => {
+        Article.findOne( { title: req.params.articleTitle }, ( error, foundArticle ) => {
+            if ( foundArticle )
+                res.send( { error: false, message: "Article found", data : foundArticle } );
+            else 
+                res.send( { error: false, message: "Oups... No article matching :(" } );
+        } );
+    
+    } );
+
 
 let APP_PORT = process.env.PORT;
 if ( APP_PORT == null || APP_PORT == "" ) 
